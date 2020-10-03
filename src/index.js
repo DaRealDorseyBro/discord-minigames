b1df4d014ae44fe6f35c95a4d0f00c19afe2e964
Rosey <65787960+DaRealDorseyBro@users.noreply.github.com>
2020-10-03 09:42:35 -0700

    discord-minigames 🌹

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 //BATTLES
 const {createBattle} = require("./battle")
 exports.startBattle = async function (member, message) { return createBattle(member, message) }
+//I SPYS
+const {createISpy} = require("./ispy")
+exports.startISpy = async function (member, message) { return createISpy(member, message) }
